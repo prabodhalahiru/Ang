cb43949eecedb134ab13ffe9fac6fc33ce8bbd25
Lahiru <prabodhalahiru119@gmail.com>
2024-11-01 23:42:29 +0530

    Reading data

--- ClientApp/src/app/components/books/books.component.ts
+++ ClientApp/src/app/components/books/books.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../../interfaces/book';
+import { BookService } from 'src/app/services/book.service';
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
-  public books: Book[] = []; // Add this line;
+  public books: Book[] = [];
 
-  constructor() {}
+  constructor(private service: BookService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.service.getAllBooks().subscribe(data => {
+      this.books = data;
+    });
+  }
 
 }
--- ClientApp/src/app/services/book.service.ts
+++ ClientApp/src/app/services/book.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Book } from '../interfaces/book';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
+  _baseUrl: string = 'api/Books';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getAllBooks() {
+    return this.http.get<Book[]>(this._baseUrl+'/GetBooks');
+  }
 }
